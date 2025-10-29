e7ccfe6e28e2dc876ff3f11f7a4ec374676db7e5
Vinodh Kumar <vinodh@outlook.com>
2025-10-28 23:56:40 -0400

    chore: align expo config and ignore build artifacts

--- projection/apps/mobile/app.config.ts
+++ projection/apps/mobile/app.config.ts
@@ -1,50 +1,29 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  name: 'Nestly',
-  slug: 'nestly',
-  version: '1.0.0',
-  orientation: 'portrait',
-  icon: './assets/icon3.png',
-  userInterfaceStyle: 'automatic',
-  scheme: 'nestly',
-  splash: {
-    image: './assets/icon3.png',
-    resizeMode: 'contain',
-    backgroundColor: '#4ABDAC'
-  },
-  assetBundlePatterns: ['**/*'],
-  ios: {
-    supportsTablet: true,
-    bundleIdentifier: 'com.w3force.nestly',
-    buildNumber: '1',
-    infoPlist: {
-      NSCameraUsageDescription: 'Nestly does not use the camera.',
-      NSPhotoLibraryUsageDescription: 'Nestly does not access your photo library.'
-    }
-  },
-  android: {
-    adaptiveIcon: {
-      foregroundImage: './assets/icon3.png',
-      backgroundColor: '#4ABDAC'
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const {
+    orientation: _orientation,
+    icon: _icon,
+    scheme: _scheme,
+    userInterfaceStyle: _userInterfaceStyle,
+    splash: _splash,
+    ios: _ios,
+    android: _android,
+    plugins: _plugins,
+    ...rest
+  } = config ?? {};
+
+  return {
+    ...rest,
+    name: 'Nestly',
+    slug: 'nestly',
+    version: '1.0.0',
+    assetBundlePatterns: ['**/*'],
+    updates: {
+      fallbackToCacheTimeout: 0
     },
-    package: 'com.w3force.nestly',
-    versionCode: 1,
-    permissions: []
-  },
-  web: {
-    favicon: './assets/favicon.png',
-    bundler: 'metro'
-  },
-  plugins: [
-    'expo-asset',
-    'expo-font'
-  ],
-  updates: {
-    fallbackToCacheTimeout: 0
-  },
-  runtimeVersion: {
-    policy: 'sdkVersion'
-  }
-});
+    runtimeVersion: {
+      policy: 'sdkVersion'
+    }
+  };
+};
